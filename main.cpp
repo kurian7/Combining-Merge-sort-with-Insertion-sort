668a67b7409b0d2fda2c6b96d2757e1ebff60bf4
Hamed Ahangari <hamed.a1373@live.com>
2014-11-25 20:33:21 +0000

    Update main.cpp
    Adding comments

--- main.cpp
+++ main.cpp
@@ -9,17 +9,22 @@ void mergeSort(int array[], int min, int max, int threshold);
 
 int main()
 {
-	const int threshold = 10;
-	const int size = 100;
-	int array[size];
-	srand(time(NULL));
-	for (int i = 0; i < size; i++)
-		array[i] = rand() % size;
-
-	mergeSort(array, 0, size - 1, threshold);
-	for (int i = 0; i < size - 1; i++){
-		cout << array[i] << " ,";
-	}
-	cin.get();
-	return 0;
+    const int threshold = 10;
+    const int size = 100;
+    int array[size];
+ 
+    srand(time(NULL));
+ 
+    //Populating array
+    for (int i = 0 ; i < size  ; i++)
+       array[i] = rand() % size;
+    
+    mergeSort(array, 0, size - 1, threshold);
+ 
+    //Printing array
+    for (int i = 0; i < size - 1; i++){
+       cout << array[i] << " ," ;
+    }
+ 
+    return 0;
 }
